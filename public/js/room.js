ccd37aa062ac181e60544b2576e0eee524f77539
Mehmetcan Çaycı <159884764+Mehmetcan1836@users.noreply.github.com>
2025-06-19 00:15:15 +0300

    sasas

--- public/js/room.js
+++ public/js/room.js
@@ -11,7 +11,7 @@ let currentUser = {
     isHost: false
 };
 let roomId = window.location.pathname.split('/').pop();
-let localStream = null;
+
 let screenStream = null;
 let nextPageToken = '';
 let isSearching = false;
